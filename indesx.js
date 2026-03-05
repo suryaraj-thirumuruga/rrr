41a3cd7e6924c977ef9e08aef6742f72afeba1e0
suryaraj-thirumuruga <180097375+suryaraj-thirumuruga@users.noreply.github.com>
2026-03-05 20:33:59 +0530

    first commit

---
+++ indesx.js
@@ -0,0 +1,16 @@
+console.log("hello welcome.");
+// document.writeln("<h1>Hello welcome...</h1>")
+var Name = "Suryaraj";
+var Name ="qwertyuio";
+
+let nameOne = "Chennai";
+nameOne =90;
+
+const nums = 123;
+nums=99;
+
+
+
+
+
+
