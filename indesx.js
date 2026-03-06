f496ad18b831398baf6b939cde701e348641e540
suryaraj-thirumuruga <180097375+suryaraj-thirumuruga@users.noreply.github.com>
2026-03-06 20:13:51 +0530

    day 2

--- indesx.js
+++ indesx.js
@@ -1,13 +1,152 @@
-console.log("hello welcome.");
-// document.writeln("<h1>Hello welcome...</h1>")
-var Name = "Suryaraj";
-var Name ="qwertyuio";
+// console.log("hello welcome.");
+// // document.writeln("<h1>Hello welcome...</h1>")
+// var Name = "Suryaraj";
+// var Name ="qwertyuio";
 
-let nameOne = "Chennai";
-nameOne =90;
+// let nameOne = "Chennai";
+// nameOne =90;
 
-const nums = 123;
-nums=99;
+// const nums = 123;
+// // nums=99;
+
+
+
+// // datatype 
+// // BOM
+// // alert("");
+// // window.prompt("Enter your name ")
+// // confirm("r u exit this page ")
+// // print("")
+// // close()
+// // open("https://www.youtube.com/")
+
+// // type casting
+
+// // Name = "10";
+
+// // number = parseInt(Name)
+
+// // toString
+
+// // String Methods 
+
+// Name ="  Suryarj  tt ";
+// console.log(Name[0]);
+// console.log(Name.indexOf("r"))
+// console.log(Name.lastIndexOf('r'))
+// Name[14]="welcome";
+// console.log(Name.concat(" welcome."))
+// console.log(Name+"qwertyu")
+// console.log(Name.length);
+// console.log(Name.repeat(5));
+// console.log(Name.slice(0,5));
+// console.log(Name.split());
+// console.log(Name.toLowerCase());
+// console.log(Name.trim());
+ 
+// // Math methods
+
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.abs(-2));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(12.33));
+//  console.log(Math.max(90,80));
+//  console.log(Math.min(80,99));
+//  console.log(Math.pow(2,5));
+//  console.log(Math.random());
+//  console.log(Math.round(4.5));
+//  console.log(Math.sign(10));
+//  console.log(Math.sqrt(64));
+
+
+
+//  // arithmetic
+// //  + - /  * %
+
+// // Assignment
+// // = += -= /= *=
+
+// // comp
+// // == === >< <= >= != 
+
+// // logic
+// // & | ~
+// // ++ --
+
+// // value ? true : false
+
+// a=10;
+// a == "10"
+// a === 10
+
+// if(10<50){
+//     console.log("one");    
+// }
+// else if((40<60)&60<80){
+//     console.log("two crt");    
+// }
+// else{
+//     console.log("zero");    
+// }
+// if(50<60){
+//     if(60<80){
+//         console.log("one");}
+// }
+
+// let y =10
+// switch(y){
+//     case 0:
+//         console.log("zero");
+//         break;
+//     case 1:
+//         console.log("one");
+//         break;
+//     case 2:
+//         console.log("two");
+//         break;
+//     default :
+//     console.error("error");             
+// }
+
+
+ 
+
+// javscript program to find maximum between two numbers.
+
+// javscript program to find maximum between three numbers.
+
+// javscript program to check whether a number is negative, positive or zero.
+
+// javscript program to check whether a number is divisible by 5 and 11 or not.
+
+// javscript program to check whether a number is even or odd.
+
+// javscript program to check whether a year is leap year or not.
+
+
+// javscript program to input any alphabet and check whether it is vowel or consonant.
+
+// javscript program to input any character and check whether it is alphabet, digit or special character.
+
+
+// javscript program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+// Percentage >= 90% : Grade A
+// Percentage >= 80% : Grade B
+// Percentage >= 70% : Grade C
+// Percentage >= 60% : Grade D
+// Percentage >= 40% : Grade E
+// Percentage < 40% : Grade F
+
+
+ 
+
+
+
+
+
+
+
 
 
 
