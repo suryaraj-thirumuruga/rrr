35224539015fc3d5217833c6c46e91851155fded
suryaraj-thirumuruga <180097375+suryaraj-thirumuruga@users.noreply.github.com>
2026-03-10 19:44:20 +0530

    day4

--- indesx.js
+++ indesx.js
@@ -1,211 +1,243 @@
-// console.log("hello welcome.");
-// // document.writeln("<h1>Hello welcome...</h1>")
-// var Name = "Suryaraj";
-// var Name ="qwertyuio";
+// // console.log("hello welcome.");
+// // // document.writeln("<h1>Hello welcome...</h1>")
+// // var Name = "Suryaraj";
+// // var Name ="qwertyuio";
 
-// let nameOne = "Chennai";
-// nameOne =90;
+// // let nameOne = "Chennai";
+// // nameOne =90;
 
-// const nums = 123;
-// // nums=99;
+// // const nums = 123;
+// // // nums=99;
 
 
 
-// // datatype 
-// // BOM
-// // alert("");
-// // window.prompt("Enter your name ")
-// // confirm("r u exit this page ")
-// // print("")
-// // close()
-// // open("https://www.youtube.com/")
+// // // datatype 
+// // // BOM
+// // // alert("");
+// // // window.prompt("Enter your name ")
+// // // confirm("r u exit this page ")
+// // // print("")
+// // // close()
+// // // open("https://www.youtube.com/")
 
-// // type casting
+// // // type casting
 
-// // Name = "10";
+// // // Name = "10";
 
-// // number = parseInt(Name)
+// // // number = parseInt(Name)
 
-// // toString
+// // // toString
 
-// // String Methods 
+// // // String Methods 
 
-// Name ="  Suryarj  tt ";
-// console.log(Name[0]);
-// console.log(Name.indexOf("r"))
-// console.log(Name.lastIndexOf('r'))
-// Name[14]="welcome";
-// console.log(Name.concat(" welcome."))
-// console.log(Name+"qwertyu")
-// console.log(Name.length);
-// console.log(Name.repeat(5));
-// console.log(Name.slice(0,5));
-// console.log(Name.split());
-// console.log(Name.toLowerCase());
-// console.log(Name.trim());
+// // Name ="  Suryarj  tt ";
+// // console.log(Name[0]);
+// // console.log(Name.indexOf("r"))
+// // console.log(Name.lastIndexOf('r'))
+// // Name[14]="welcome";
+// // console.log(Name.concat(" welcome."))
+// // console.log(Name+"qwertyu")
+// // console.log(Name.length);
+// // console.log(Name.repeat(5));
+// // console.log(Name.slice(0,5));
+// // console.log(Name.split());
+// // console.log(Name.toLowerCase());
+// // console.log(Name.trim());
  
-// // Math methods
+// // // Math methods
 
-// console.log(Math.E);
-// console.log(Math.PI);
-// console.log(Math.abs(-2));
-// console.log(Math.ceil(4.1));
-// console.log(Math.floor(12.33));
-//  console.log(Math.max(90,80));
-//  console.log(Math.min(80,99));
-//  console.log(Math.pow(2,5));
-//  console.log(Math.random());
-//  console.log(Math.round(4.5));
-//  console.log(Math.sign(10));
-//  console.log(Math.sqrt(64));
+// // console.log(Math.E);
+// // console.log(Math.PI);
+// // console.log(Math.abs(-2));
+// // console.log(Math.ceil(4.1));
+// // console.log(Math.floor(12.33));
+// //  console.log(Math.max(90,80));
+// //  console.log(Math.min(80,99));
+// //  console.log(Math.pow(2,5));
+// //  console.log(Math.random());
+// //  console.log(Math.round(4.5));
+// //  console.log(Math.sign(10));
+// //  console.log(Math.sqrt(64));
 
 
 
-//  // arithmetic
-// //  + - /  * %
+// //  // arithmetic
+// // //  + - /  * %
 
-// // Assignment
-// // = += -= /= *=
+// // // Assignment
+// // // = += -= /= *=
 
-// // comp
-// // == === >< <= >= != 
+// // // comp
+// // // == === >< <= >= != 
 
-// // logic
-// // & | ~
-// // ++ --
+// // // logic
+// // // & | ~
+// // // ++ --
 
-// // value ? true : false
+// // // value ? true : false
 
-// a=10;
-// a == "10"
-// a === 10
+// // a=10;
+// // a == "10"
+// // a === 10
 
-// if(10<50){
-//     console.log("one");    
-// }
-// else if((40<60)&60<80){
-//     console.log("two crt");    
-// }
-// else{
-//     console.log("zero");    
-// }
-// if(50<60){
-//     if(60<80){
-//         console.log("one");}
-// }
+// // if(10<50){
+// //     console.log("one");    
+// // }
+// // else if((40<60)&60<80){
+// //     console.log("two crt");    
+// // }
+// // else{
+// //     console.log("zero");    
+// // }
+// // if(50<60){
+// //     if(60<80){
+// //         console.log("one");}
+// // }
 
-// let y =10
-// switch(y){
-//     case 0:
-//         console.log("zero");
-//         break;
-//     case 1:
-//         console.log("one");
-//         break;
-//     case 2:
-//         console.log("two");
-//         break;
-//     default :
-//     console.error("error");             
-// }
+// // let y =10
+// // switch(y){
+// //     case 0:
+// //         console.log("zero");
+// //         break;
+// //     case 1:
+// //         console.log("one");
+// //         break;
+// //     case 2:
+// //         console.log("two");
+// //         break;
+// //     default :
+// //     console.error("error");             
+// // }
 
 
  
 
-// javscript program to find maximum between two numbers.
+// // javscript program to find maximum between two numbers.
 
-// javscript program to find maximum between three numbers.
+// // javscript program to find maximum between three numbers.
 
-// javscript program to check whether a number is negative, positive or zero.
+// // javscript program to check whether a number is negative, positive or zero.
 
-// javscript program to check whether a number is divisible by 5 and 11 or not.
+// // javscript program to check whether a number is divisible by 5 and 11 or not.
 
-// javscript program to check whether a number is even or odd.
+// // javscript program to check whether a number is even or odd.
 
-// javscript program to check whether a year is leap year or not.
-
-
-// javscript program to input any alphabet and check whether it is vowel or consonant.
-
-// javscript program to input any character and check whether it is alphabet, digit or special character.
+// // javscript program to check whether a year is leap year or not.
 
 
-// javscript program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
-// Percentage >= 90% : Grade A
-// Percentage >= 80% : Grade B
-// Percentage >= 70% : Grade C
-// Percentage >= 60% : Grade D
-// Percentage >= 40% : Grade E
-// Percentage < 40% : Grade F
+// // javscript program to input any alphabet and check whether it is vowel or consonant.
+
+// // javscript program to input any character and check whether it is alphabet, digit or special character.
 
 
- let array = [1,2,3,4,'s',"surya"];
- console.log(array);
- console.log(array[0]);
- console.log(array.concat(array));
- console.log(array.indexOf(1));
- array[6]="chennai";
- array.push(90)
- array.push(91)
- array.push(92)
- array.pop()
- array.unshift("one")
- array.unshift("Two")
- array.shift()
- array.splice(2,1,"KIA")
- console.log(array);
-
-multiArray = [[1,2,3,4,5,6],[7,8,9,9,[5,6,7,8,'s',[0,9,7,'t']]]]
-console.log(multiArray[1][4][5][3]);
+// // javscript program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+// // Percentage >= 90% : Grade A
+// // Percentage >= 80% : Grade B
+// // Percentage >= 70% : Grade C
+// // Percentage >= 60% : Grade D
+// // Percentage >= 40% : Grade E
+// // Percentage < 40% : Grade F
 
 
-const Array = [];
-Array.push(90);
-console.log(Array);
+//  let array = [1,2,3,4,'s',"surya"];
+//  console.log(array);
+//  console.log(array[0]);
+//  console.log(array.concat(array));
+//  console.log(array.indexOf(1));
+//  array[6]="chennai";
+//  array.push(90)
+//  array.push(91)
+//  array.push(92)
+//  array.pop()
+//  array.unshift("one")
+//  array.unshift("Two")
+//  array.shift()
+//  array.splice(2,1,"KIA")
+//  console.log(array);
 
-let obj ={
-    Name :"Suryaraj",
-    age:12,
-    city:"Erode",
-    brother:{
-        Name:"Karthick",
-        age :10
-    }
-}
-console.log(obj.brother.Name);
+// multiArray = [[1,2,3,4,5,6],[7,8,9,9,[5,6,7,8,'s',[0,9,7,'t']]]]
+// console.log(multiArray[1][4][5][3]);
 
-ArryaOfObject =[
-    {Name:"Suryraj",age:21},
-    {Name:"Suryraj",age:22},
-    {Name:"Suryraj",age:23},
-    {Name:"Suryraj",age:24},
-    {Name:"Suryraj",age:25},
-]
-console.log(ArryaOfObject[3].age);
-let ayy=['a','b','c','d','e'];
 
-for(let i=0;i<ayy.length;i++){
-    console.log(ayy[i]);   
-}
+// const Array = [];
+// Array.push(90);
+// console.log(Array);
 
-let u =5
-let r=10
-while (u<10) {
-    u++;
-    console.log(u);   }
+// let obj ={
+//     Name :"Suryaraj",
+//     age:12,
+//     city:"Erode",
+//     brother:{
+//         Name:"Karthick",
+//         age :10
+//     }
+// }
+// console.log(obj.brother.Name);
 
-do{
-    r++;
-    console.log(r);
+// ArryaOfObject =[
+//     {Name:"Suryraj",age:21},
+//     {Name:"Suryraj",age:22},
+//     {Name:"Suryraj",age:23},
+//     {Name:"Suryraj",age:24},
+//     {Name:"Suryraj",age:25},
+// ]
+// console.log(ArryaOfObject[3].age);
+// let ayy=['a','b','c','d','e'];
+
+// for(let i=0;i<ayy.length;i++){
+//     console.log(ayy[i]);   
+// }
+
+// let u =5
+// let r=10
+// while (u<10) {
+//     u++;
+//     console.log(u);   }
+
+// do{
+//     r++;
+//     console.log(r);
     
-}while(r<15)
+// }while(r<15)
 
-ayy.forEach((r)=>{console.log(r);})
-ayy.map((y)=>{console.log(y);})
+// ayy.forEach((r)=>{console.log(r);})
+// ayy.map((y)=>{console.log(y);})
 
-for(let i of ayy){
-    console.log(i);    
-}
+// for(let i of ayy){
+//     console.log(i);    
+// }
+
+
+// function Home(city){
+//     console.log("welocme to the Home at "+city);
+//     let y=0
+// }
+
+// Home("Chennai");
+
+
+// function Home1(){
+//     return 24+24;
+// }
+
+// console.log(Home1());
+
+
+// const Name =()=>{
+//     console.log("Arrow Function");   
+// }
+// Name();
+
+
+// setTimeout(()=>{
+//     console.log("Time Out");
+//     clearInterval(y)
+// },4000)
+
+// let y= setInterval(() => {
+//     console.log("Intervel");    
+// }, 2000);
+
 
 
 
